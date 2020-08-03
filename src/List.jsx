20fafae06f48cb28c62a483c2c49f9bcd2bf16d3
boredom101 <ymnewmark@gmail.com>
2020-08-03 16:58:50 -0400

    now it saves to site

--- src/List.jsx
+++ src/List.jsx
@@ -7,13 +7,14 @@ import {
 } from '@getflywheel/local-components';
 import { confirm } from '@getflywheel/local/renderer';
 import Task from './Task';
+import { ipcRenderer } from 'electron';
 
 export default class List extends Component {
     constructor(props) {
         super(props);
         this.state = {
             textareaValue: '',
-            items: []
+            items: this.fetchTasks()
         };
         this.onTextareaChange = this.onTextareaChange.bind(this);
 		this.onTextareaKeyPress = this.onTextareaKeyPress.bind(this);
@@ -21,7 +22,7 @@ export default class List extends Component {
 
     addTask(text) {
         const items = this.state.items.concat([{text: text}]);
-        this.setState({items: items, textareaValue: ''});
+        this.setState({items: items, textareaValue: ''}, this.syncTasks);
     }
 
     onTextareaChange(event) {
@@ -29,7 +30,11 @@ export default class List extends Component {
 			textareaValue: event.target.value,
 		});
     }
-    
+
+    syncTasks() {
+        ipcRenderer.send("save-todo-list", this.props.site.id, this.state.items);
+    }
+
     onTextareaKeyPress(event) {
 		if (event.key !== 'Enter' || event.altKey || event.shiftKey) {
 			return;
@@ -48,9 +53,21 @@ export default class List extends Component {
             items.splice(items.findIndex((value) => value.text == item.text), 1);
             this.setState({
                 items: items
-            })
+            }, this.syncTasks);
         });
     }
+    
+    fetchTasks() {
+        return this.props.site.todo || [];
+    }
+
+    componentDidUpdate(previousProps) {
+		if (previousProps.site.id !== this.props.site.id) {
+			this.setState({
+				items: this.fetchTasks(),
+			});
+		}
+	}
 
     render() {
         return <InnerPaneSidebar>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import run from './runner';
+import { SiteData } from '@getflywheel/local/main';
 
 export default function(context) {
     const { notifier, electron } : { notifier: any, electron: typeof Electron } = context;
 
     electron.ipcMain.on("run-todo-task", (event, site, data) => {
         run(site, data);
+    });
+    electron.ipcMain.on("save-todo-list", (event, site, data) => {
+        SiteData.updateSite(site, {
+            id: site,
+            todo: data
+        })
     });
 }
